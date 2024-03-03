b35584b908a95e22aea65c1642e4e3c1a378b9d1
Kyle Hughes <Hugh472@users.noreply.github.com>
2024-03-03 18:59:40 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #usr/local/env bash
 
 # Install gh
-GITHUB\*CLI\*VERSION=$(curl -s "https://api.github.com/repos/cli/cli/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+GITHUB_CLI_VERSION=$(curl -s "https://api.github.com/repos/cli/cli/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
 cd
 curl -Lo gh.deb "https://github.com/cli/cli/releases/latest/download/gh_${GITHUB_CLI_VERSION}\_linux_armv6.deb"
 sudo dpkg -i gh.deb
